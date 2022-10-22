f7fedfb1f9d1bc628f318dc7625c291df47019da
egespindola <eds.gesp@gmail.com>
2022-10-22 17:13:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+
+
+  return (
+    <>
+      <h1 className='font-bold text-2xl'>Hello World</h1>
+      <button className='bg-cyan-300 text-white font-bold rounded px-2 hover:bg-cyan-500'>Send</button>
+    </>
+  )
+}
+
